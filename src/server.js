b0a88f3c762aecd94b7f494cc15a578ec4dcb389
Tilde Ann Thurium <tilde@thuryism.net>
2020-08-23 11:50:13 -0700

    add route for adding participants to airtable base

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const http = require("http");
 const express = require("express");
-const path = require("path");
 
 const app = express();
+
+// you don't actually need body-parser to parse url encoded bodies these days. dope!
+app.use(express.urlencoded());
 
 app.get("/", (request, response) => {
   response.send(
     "hello world. time is a flat circle. the end is the beginning is the end."
   );
+});
+
+// add a new participant to the Airtable base
+app.post("/api/participants", (request, response) => {
+  console.log(request.body);
+  response.status(200).send("niiice 💟");
 });
 
 // palindromes aren't loops but at least they take you back to the beginning.
