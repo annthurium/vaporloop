3bdd4cfefb08946e0b819dcb37f80ec32679f85c
Ekate Kuznetsova <ekate@tokentransit.com>
2020-10-10 01:04:06 -0700

    x-offset and aol subtree
    Added the AOL subtree.
    
    Managing subtree locations manually was gross, so our friend the x-offset makes an appearence.

--- phone_tree_generator/generator_from_config.py
+++ phone_tree_generator/generator_from_config.py
@@ -4,29 +4,27 @@ import generate_event
 # These constants let us keep track of rows instead of coordinates, which makes rendering easier.
 Y_CONST = 400
 X_CONST = 300
+
 # Generate states using these constants
-def getStates(f):
-    f["x"] = f["x"] * X_CONST
+def getStates(f, x):
+    f["x"] = (f["x"] + x)  * X_CONST
     f["y"] = f["y"] * Y_CONST
     # Generate states. The stupid bonus parameter is the main menu event.
     return generate_event.GenerateStates(f, "main_menu") 
 
+# Infrastructure for loading config files
+states = []
+def load(filename):
+  with open(filename) as f:
+    data = json.load(f)
+    for s in data["states"]:
+      states.extend(getStates(s, data["x-offset"]))
+
 
 # Load all our config files. This should be a config file!
-with open('main_menu.json') as f:
-  mainmenu = json.load(f)
-with open('pigeon.json') as f:
-  pigeon = json.load(f)
-
-
-### XXX: what to do with the "end" and how to hardcode that in
-states = []
-
-for f in mainmenu:
-    states.extend(getStates(f))
-
-for f in pigeon:
-    states.extend(getStates(f))
+load("main_menu.json")
+load("pigeon.json")
+load("aol.json")
 
 # Generate the flow. The stupid bonus parameter is the top-most y-coordinate for the "trigger" event
 print json.dumps(generate_event.GenerateFlow(states, -2 * Y_CONST), indent=4)
